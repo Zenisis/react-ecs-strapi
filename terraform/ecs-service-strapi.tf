01de7b5a4767a0d651517eff41cae000cacb782a
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-07-12 15:41:07 +0530

    Create ecs-service-strapi.tf

---
+++ terraform/ecs-service-strapi.tf
@@ -0,0 +1,16 @@
+resource "aws_ecs_service" "strapi" {
+  name            = "strapi-service"
+  cluster         = var.cluster_id
+  task_definition = aws_ecs_task_definition.strapi_task_definition.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [var.security_group_id]
+    assign_public_ip = true
+  }
+}
+
+output "private_ip" {
+  value = aws_ecs_service.strapi.network_configuration[0].subnets[0]
+}

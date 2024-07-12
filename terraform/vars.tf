23e51dff0942db54fe51901df31749cc5eac5d49
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-07-12 15:37:11 +0530

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the infrastructure"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
